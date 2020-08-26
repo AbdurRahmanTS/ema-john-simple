0338fc8fe28e17287d4a269e21502e49544f20fe
Abdur Rahman <abdurrahmants@gmail.com>
2020-08-27 00:02:50 +0600

    solved key error

--- src/components/Shop/Shop.js
+++ src/components/Shop/Shop.js
@@ -6,7 +6,7 @@ import Cart from '../Cart/Cart';
 
 const Shop = () => {
     const first10 = fakeData.slice(0,10);
-    const [products, setProducts] = useState(first10);
+    const [products] = useState(first10);
     const [cart, setCart] = useState([]);
 
     const handleAddProduct = (product) => {
@@ -17,7 +17,7 @@ const Shop = () => {
         <div className="shop-container">
             <div className="product-container">
                     {
-                        products.map(products => <Product handleAddProduct = {handleAddProduct} product = {products}></Product>)
+                        products.map(products => <Product key ={products.key} handleAddProduct = {handleAddProduct} product = {products}></Product>)
                     }
             </div>
             <div className="cart-container">
